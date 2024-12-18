1bea16623e7e095509646ea279c6c9921e414ff1
Cinzia88 <marraffacinzia5@gmail.com>
2024-12-18 22:30:43 +0100

    update

--- database/migrations/2024_12_02_102309_create_bookings_table.php
+++ database/migrations/2024_12_02_102309_create_bookings_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->foreignId('slot_id')->cascadeOnDelete();
             $table->date('data');
             $table->string('messaggio')->nullable();
-            $table->boolean('available')->default(true);
             $table->timestamps();
         });
     }
